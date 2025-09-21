755a42949752ef5415bfa64792ab819035a689cf
Velyn-N <nils01.gaming@gmail.com>
2025-09-21 23:58:41 +0200

    Improved UI Performance by reducing shown entries

--- src/main/java/me/velyn/mcactivitymonitor/ui/QuteService.java
+++ src/main/java/me/velyn/mcactivitymonitor/ui/QuteService.java
@@ -46,9 +46,61 @@ public class QuteService {
 
     public Map<LocalDate, List<ActivityRecord>> groupByDate(List<ActivityRecord> records) {
         return records.stream()
-                      .collect(Collectors.groupingBy(
-                              ar -> ar.recordCreationTime.toLocalDate(),
-                              LinkedHashMap::new,
-                              Collectors.toList()));
+                .collect(Collectors.groupingBy(
+                        ar -> ar.recordCreationTime.toLocalDate(),
+                        LinkedHashMap::new,
+                        Collectors.toList()));
+    }
+
+    public List<ActivityRecord> accumulateByHour(List<ActivityRecord> records) {
+        List<ActivityRecord> accumulates = new ArrayList<>();
+
+        LocalDateTime currHour = null;
+        List<ActivityRecord> workingSet = new ArrayList<>();
+
+        for (var record : records) {
+            LocalDateTime recordHour = atStartOfHour(record.dataRetrievalTime);
+
+            if (currHour == null) {
+                currHour = recordHour;
+            }
+
+            if (recordHour.isEqual(currHour)) {
+                workingSet.add(record);
+            } else {
+                accumulates.add(processHourGroup(workingSet, currHour));
+
+                currHour = recordHour;
+                workingSet = new ArrayList<>();
+                workingSet.add(record);
+            }
+        }
+
+        accumulates.add(processHourGroup(workingSet, currHour));
+
+        return accumulates;
+    }
+
+    private ActivityRecord processHourGroup(List<ActivityRecord> workingSet, LocalDateTime hour) {
+        if (workingSet.isEmpty()) return null;
+        
+        double averagePlayers = workingSet.stream()
+                .mapToInt(ar -> ar.playerCount)
+                .average().orElse(0.0);
+        
+        boolean wasOnline = workingSet.stream().allMatch(ar -> ar.online);
+        
+        ActivityRecord acc = new ActivityRecord();
+        acc.recordCreationTime = hour;
+        acc.dataRetrievalTime = hour;
+        acc.online = wasOnline;
+        acc.server = workingSet.getFirst().server;
+        acc.playerCount = (int) Math.round(averagePlayers);
+        
+        return acc;
+    }
+    
+    private LocalDateTime atStartOfHour(LocalDateTime ldt) {
+        return LocalDateTime.of(ldt.toLocalDate(), LocalTime.of(ldt.getHour(), 0));
     }
 }
